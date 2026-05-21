bddbcb438120b6a82e61ac1ff0a6aa8dbd1a8060
Nuria Calo <nuriacalomosquera@gmail.com>
2026-05-21 22:28:32 +0200

    chore(frontend): actualiza baseUrl local y dependencias

--- frontend/between_pages/lib/core/constants/api_constants.dart
+++ frontend/between_pages/lib/core/constants/api_constants.dart
@@ -1,9 +1,8 @@
 class ApiConstants {
   static const String baseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue:'http://10.86.122.198:8080/api', // Usa el IP de tu PC (10.86.122.198). Ajústalo según red/emulador.
-    // defaultValue: 'http://192.168.0.15:8080/api',
-
+    // Usa la IP de tu red local (192.168.0.15). Ajústalo si cambia tu red.
+    defaultValue: 'http://192.168.0.15:8080/api',
   );
 
   // JIKAN API (MyAnimeList - Unofficial)
