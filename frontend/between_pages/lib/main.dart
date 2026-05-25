1c4a1728bfb93b8849d2462d66a05f1fd8391d28
Nuria Calo <nuriacalomosquera@gmail.com>
2026-05-25 12:20:28 +0200

    chore(frontend): Update dependencies and main application configuration - pubspec.yaml: Update Flutter dependencies with new packages - pubspec.lock: Lock dependency versions - main.dart: Improve app initialization and configuration

--- frontend/between_pages/lib/main.dart
+++ frontend/between_pages/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:between_pages/core/providers/locale_provider.dart';
 import 'package:between_pages/core/providers/theme_provider.dart';
 import 'package:between_pages/core/router/app_router.dart';
 import 'package:between_pages/l10n/app_localizations.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/theme/app_theme.dart';
@@ -22,8 +23,22 @@ class BetweenPagesApp extends ConsumerWidget {
     return MaterialApp.router(
       title: 'Between Pages',
       debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
+      theme: AppTheme.lightTheme.copyWith(
+        pageTransitionsTheme: const PageTransitionsTheme(
+          builders: {
+            TargetPlatform.android: PredictiveBackPageTransitionsBuilder(),
+            TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+          },
+        ),
+      ),
+      darkTheme: AppTheme.darkTheme.copyWith(
+        pageTransitionsTheme: const PageTransitionsTheme(
+          builders: {
+            TargetPlatform.android: PredictiveBackPageTransitionsBuilder(),
+            TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+          },
+        ),
+      ),
       themeMode: themeMode,
       routerConfig: router,
       // Usamos las delegaciones y locales autogenerados por Flutter
